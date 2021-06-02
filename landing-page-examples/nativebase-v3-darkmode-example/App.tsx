ade8a21ada75774b9d383e587c0a61f5dabd79a6
cyrus25 <cyruspassi@gmail.com>
2021-06-02 18:01:24 +0530

    dark mode

--- landing-page-examples/nativebase-v3-darkmode-example/App.tsx
+++ landing-page-examples/nativebase-v3-darkmode-example/App.tsx
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import {
+  Heading,
+  useColorMode,
+  Button,
+  HStack,
+  Avatar,
+  Center,
+  useColorModeValue,
+  NativeBaseProvider,
+} from "native-base";
 
-export default function App() {
+function HomeScreen() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const mode = searchParams.get("mode");
+    if (mode === "dark") {
+      toggleColorMode();
+    }
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Center flex={1} bg={useColorModeValue("gray.200", "gray.800")}>
+      <Heading>I'm a Heading</Heading>
+      <Button
+        colorScheme={colorMode === "light" ? "blue" : "red"}
+        onPress={() => {
+          toggleColorMode();
+        }}
+      >
+        Change mode
+      </Button>
+      <HStack space={2} mt={3}>
+        <Avatar
+          name="Ankur"
+          borderWidth={2}
+          source={{
+            uri:
+              "https://pbs.twimg.com/profile_images/1309797238651060226/18cm6VhQ_400x400.jpg",
+          }}
+        />
+        <Avatar
+          name="Rohit"
+          borderWidth={2}
+          source={{
+            uri:
+              "https://pbs.twimg.com/profile_images/1352844693151731713/HKO7cnlW_400x400.jpg",
+          }}
+        />
+      </HStack>
+    </Center>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <HomeScreen />
+    </NativeBaseProvider>
+  );
+}
